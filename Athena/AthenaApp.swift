846520f44c0833af33d217617f1ce3c17f6dabda
Woo Jia Hao <woojiahao1234@gmail.com>
2022-12-04 21:32:35 +0800

    Initial Commit

---
+++ Athena/AthenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthenaApp.swift
+//  Athena
+//
+//  Created by Woo Jia Hao on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AthenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athena/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Athena
+//
+//  Created by Woo Jia Hao on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
